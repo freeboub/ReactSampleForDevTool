4228e294733deb57c7574d00ecfa356942625031
Olivier Bouillet <freeboub@gmail.com>
2024-04-21 15:59:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, {useEffect, useState} from 'react';
+
+export const timeout = 100;
+
+/* component which is too long to render */
+const HeavyComponent = () => {
+  const start = performance.now()
+  let i = 0;
+  while (i < 9000000) {
+    i = i + 1;
+  }
+  console.log('render time', performance.now() - start)
+  return null;
+};
+
+/* core of the App */
+
+const App = () => {
+  
+  // eslint-disable-next-line no-unused-vars
+  const [_time, setTime] = useState(0);
+
+  // setState in an interval to for refresh periodically
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(_time => _time + 1);
+    }, timeout);
+    return () => clearInterval(interval)
+  }, []);
+
+  return <HeavyComponent />;
+};
 
 export default App;
